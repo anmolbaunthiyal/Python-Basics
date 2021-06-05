4cde6a7acff38f2ae42ef5bc4cd40901f8896f41
Anmol Baunthiyal <baunthiyalanmol@gmail.com>
2021-06-05 15:15:18 +0530

    Assign1.py

---
+++ Assign1.py
@@ -0,0 +1,49 @@
+#                                       """Assignment Number 1"""
+#Q1
+# s = "Hey i am from New Delhi"
+# print("The Length of the String is", len(s))
+# list1=s.split()
+# print(list1)
+#Q2
+# s = "name is rahul"
+# print(s.capitalize())
+# print(s.upper())
+# print(s.replace("n","r",1))
+#Q3
+# l = int(input("Enter Length"))
+# b = int(input("Enter Breadth"))
+# p = 2*(l+b)
+# print("Parameter of rectangle is",p)
+#Q4
+# dia = int(input("Enter Diameter"))
+# print("Circumfrence is ",2*3.14*(dia/2))
+#Q5
+# import cmath
+# a = int(input("Enter a"))
+# b = int(input("Enter b"))
+# c = int(input("Enter c"))
+# d=(b**2)-(4*a*c)
+# x=(-b+cmath.sqrt(d))/(2*a)
+# y=(-b-cmath.sqrt(d))/(2*a)
+# print(f"The Two Solution of the eqns are {x} and {y}")
+#Q6
+# rad = int(input("Enter Radius"))
+# vol=4/3*(3.14*rad**3)
+# print(vol)
+# Q7
+# numb=input("Enter number ")
+# print("Number of digits are ",len(numb))
+#Q8
+# str=input("Enter String :")
+# print(str.upper())
+#Q9
+# a=input("Enter String ")
+# n=int(input("Enter Index Number "))
+# c=input("Enter Character ")
+# b=list(a)
+# b[n-1]=c
+# sep=""
+# print("After Replacement:  ",sep.join(b))
+#Q10
+# strr=input("Enter string ")
+# print(strr[::-1])
